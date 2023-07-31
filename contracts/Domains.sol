ecc810065f853d85923409bce9fb2e6b0123d8fc
Sachin <sachinkshuklaoo7@gmail.com>
2023-07-31 11:27:17 +0530

    Added Set Record Feature

--- contracts/Domains.sol
+++ contracts/Domains.sol
@@ -10,9 +10,12 @@ contract Domains {
   }
 
   mapping(string => address) public domains;
+  mapping(string => string) public records;
 
   // Register Domain Name
   function register(string calldata name) public {
+    // Check if Domain is already reistered or not
+    require(domains[name] == address(0));
     domains[name] = msg.sender;
     console.log("Domain name registerd for %s", msg.sender);
   }
@@ -20,5 +23,16 @@ contract Domains {
   // Domain owners' address
   function getAddress(string calldata name) public view returns(address) {
     return domains[name];
+  }
+
+  // Set Domain Record
+  function setRecord(string calldata name, string calldata record) public {
+    require(domains[name] == msg.sender, "You can't set this record");
+    records[name] = record;
+  }
+
+  // Get Domain Record
+  function getRecord(string calldata name) public view returns(string memory) {
+    return records[name];
   }
 }
--- scripts/run.js
+++ scripts/run.js
@@ -6,14 +6,23 @@ async function main() {
     const deployedContract = await ethers.deployContract("Domains")
     await deployedContract.waitForDeployment()
 
-    console.log("Domains Contract Address:", await deployedContract.getAddress())
+    console.log(
+        "Domains Contract Address:",
+        await deployedContract.getAddress()
+    )
     console.log("Domains Contract Owned by:", owner.address)
 
-    const txn = await deployedContract.register('doom')
+    let txn = await deployedContract.register("doom")
     await txn.wait()
 
-    const domainOwner = await deployedContract.getAddress('doom')
+    const domainOwner = await deployedContract.getAddress("doom")
     console.log("Domains Contract Owned by:", domainOwner)
+
+    txn = await deployedContract
+        .connect(randomPerson)
+        .setRecord("doom", "Doom DNS Record")
+    await txn.wait()
+    console.log("task completed")
 }
 
 const runMain = async () => {
